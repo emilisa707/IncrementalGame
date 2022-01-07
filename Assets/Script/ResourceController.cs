f8234c7173afb1e6db9bf9d3babfb78b834ddb75
Emilisa <emilisa90@gmail.com>
2022-01-07 19:01:53 +0700

    updated

---
+++ Assets/Script/ResourceController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceController : MonoBehaviour
+{
+    public Button ResourceButton;
+    public Image ResourceImage;
+    public Text ResourceDescription;
+    public Text ResourceUpgradeCost;
+    public Text ResourceUnlockCost;
+
+    private ResourceConfig _config;
+
+    private int _level = 1;
+
+    private void Start()
+    {
+        ResourceButton.onClick.AddListener(UpgradeLevel);
+    }
+    public void SetConfig(ResourceConfig config)
+    {
+        _config = config;
+
+        ResourceDescription.text = $"{_config.Name} Lv.{_level}\n+{GetOutput().ToString("0")}";     //ToString("0") berfungsi untuk membuang angaka di belakang koma
+        ResourceUnlockCost.text = $"Unlock Cost \n{_config.UnlockCost}";
+        ResourceUpgradeCost.text = $"Upgrade Cost \n{GetUpgradeCost()}";
+    }
+
+    public double GetOutput()
+    {
+        return _config.Output * _level;
+    }
+
+    public double GetUpgradeCost()
+    {
+        return _config.UpgradeCost * _level;
+    }
+
+    public double GetUnlockCost()
+    {
+        return _config.UnlockCost;
+    }
+
+    public void UpgradeLevel()
+    {
+        double upgradeCost = GetUpgradeCost();
+        if(GameManager.Instance.TotalGold < upgradeCost)
+        {
+            return;
+        }
+
+        GameManager.Instance.AddGold(-upgradeCost);
+        _level++;
+
+        ResourceUpgradeCost.text = $"Upgrade Cost\n{GetUpgradeCost()}";
+        ResourceDescription.text = $"{_config.Name} lv.{_level}\n+{GetOutput().ToString("0")}";
+    }
+}
